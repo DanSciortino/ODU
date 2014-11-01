7a02a2b8ffca2649cdb1a37094c159d2d34e3a43
disco <Thegreyf0x>
2014-11-01 12:15:01 -0400

    fixing switch 1
    Issue with switch 1 duplicating cout statement.

--- CS/CS 250/Assignment 4/invesments_driver.cpp
+++ CS/CS 250/Assignment 4/invesments_driver.cpp
@@ -30,44 +30,28 @@ int main()
 
     cout << "Enter a Number 1-3 to set up a Investment Portfolio: "<< endl;
     cin >> tempt;
-    if (tempt < 1 || tempt > 3)
-        {
-            cout << "Please enter a new number: " << endl;
-            cin >> tempt;
-        }
-    while (tempt)
-        {
-            switch(tempt)
-                {
-                case 1:
-                {
-                    cout << "Enter any ticker information for the stock:" << endl;
-                    getline(cin, input);
-                    StockCurr = new Stock(input);
-                    newone = StockCurr;
+    while(tempt < 1 || tempt > 3)
+    {
+        cout << "Please enter a new number: " << endl;
+        cin >> tempt;
+    }
+while(tempt){
+    switch(tempt){
+case 1:{}break;
+case 2:{}break;
+case 3:{}break;
 
-                }
-                break;
 
-                case 2:
-                {
-                    RealEstateCurr = new RealEstate(tempi);
-                    newone = RealEstateCurr;
 
-                }
-                break;
+}
+    cout << "Enter a Number 1-3 to set up a Investment Portfolio: "<< endl;
+    cin >> tempt;
+    while(tempt < 1 || tempt > 3)
+    {
+        cout << "Please enter a new number: " << endl;
+        cin >> tempt;
+    }
 
-                case 3:
-                {
-                    cout << "Enter the name for the Collection" << endl;
-                    cin >> input;
-                    CollectionCurr = new Collection(input);
-                    newone = CollectionCurr;
-
-                }
-                break;
-                } //closes switch
-
-        }//closes loop
+}
     return 0;
 }// closes program
