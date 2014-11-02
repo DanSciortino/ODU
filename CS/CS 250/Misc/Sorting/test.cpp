631b3421b5c745e8a8cd58fe9ef1e4062098c692
disco <Thegreyf0x>
2014-11-01 23:24:59 -0400

    adding cout

--- CS/CS 250/Misc/Sorting/test.cpp
+++ CS/CS 250/Misc/Sorting/test.cpp
@@ -6,24 +6,34 @@ using namespace std;
 
 int main()
 {
-    int arraysize = 5000;
+    int arraysize = 10;
     int array1[arraysize];
     int array2[arraysize];
     int array3[arraysize];
 
 
     for(int j = 0; j < arraysize-1; j++)
-        {
-            int i = 0;
-            i = rand () % arraysize;
-            array1 [j] = i;
-            array2 [j] = i;
-            array3 [j] = i;
-        }
+    {
+        int i = 0;
+        i = rand () % arraysize;
+        array1 [j] = i;
+        array2 [j] = i;
+        array3 [j] = i;
+    }
 
+        for(int i = 0; i < arraysize; i++)
+    {
+        cout << array1[i]<< endl;
+    }
+     cout << endl;
     bubbleSort(array1,arraysize);
     selectionSort(array2,arraysize);
     insertionSort(array3,arraysize);
+
+    for(int i = 0; i < arraysize; i++)
+    {
+        cout << array1[i]<< endl;
+    }
 
     return 0;
 }
